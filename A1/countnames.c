eedc73f3abca287850e1812f3e6c8daf98ebba29
andrej-rivera <andre_rivera1337@hotmail.com>
2023-02-20 20:12:08 -0800

    Assignment 1 - Finalized edge cases & comments

--- A1/countnames.c
+++ A1/countnames.c
@@ -1,20 +1,38 @@
-/*
- Created by andre on 2/20/23.
- References:
- https://stackoverflow.com/questions/2693776/removing-trailing-newline-character-from-fgets-input
- https://cplusplus.com/reference/cstring/
- https://cplusplus.com/reference/cstdio/fgets/
+/**
+ * Description: This program takes in an file path argument from the terminal and reads it. 
+ * Given a list of names, it will count all the occurences each name appears.
+ * Author names: Andre Rivera, Tanisha Damle 
+ * Author emails: andre.rivera@sjsu.edu, tanisha.damle@sjsu.edu
+ * Last modified date: 2/20/23
+ * Creation date: 2/19/23
+ * References:
+ * - https://stackoverflow.com/questions/2693776/removing-trailing-newline-character-from-fgets-input
+ * - https://cplusplus.com/reference/cstring/
+ * - https://cplusplus.com/reference/cstdio/fgets/
 */
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 
+//This function takes in a string as an input argument
 int main(int argc, char *argv[]) {
-    char nameArray[100][30]; // create array of 100 names that can be up to 30 characters long
-
+    
+    if(argv[1] == NULL) //edge case, no argument is given
+    {
+    	return 0;
+    }
+    
+    //Open file
     FILE *textFile;
     textFile = fopen(argv[1], "r");
+    if(textFile == NULL) //edge case for when fopen fails to open a file
+    {
+    	fprintf(stderr, "error: cannot open file\n");
+    	return 1;
+    }
 
+    //Initialize variables
+    char nameArray[100][30]; // create array of 100 names that can be up to 30 characters long
     int i = 0; //line counter
     int currentSize = 0; //reflects the current size of the array
     char string[30]; //variable to hold strings read from fgets
