5a5ccb0938ba272189fe2a4b8d32945c10ec4106
TanishaD111 <tanishadamle111@gmail.com>
2023-03-05 08:23:39 +0000

    ws5 new edit

--- WS5/ws5.c
+++ WS5/ws5.c
@@ -8,8 +8,14 @@ int main(void) {
    pid_t pid;
    
    pid = fork();
+   pid = fork();
    
-   if(pid == 0){ // child process
+   if(pid < 0){ // error occurred
+      fprintf(stderr, "Fork Failed\n");
+      return 1;
+   }
+   
+   else if(pid == 0){ // child process
       printf("hello world from PID %d!\n", (int) getpid());
       return 0;
    } 
