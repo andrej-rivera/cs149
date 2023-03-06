7a38cd01b9e57104927f216a298e7d80e40c473a
andrej-rivera <andre_rivera1337@hotmail.com>
2023-03-06 01:29:50 -0800

    Added pipes & forks, functionality is not completed.

--- A2/countnames_parallel.c
+++ A2/countnames_parallel.c
@@ -18,6 +18,7 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<sys/types.h>
+#include<sys/wait.h> 
 //#include<sys/wait.h>
 //This function takes in a string as an input argument
 int main(int argc, char *argv[]) {
@@ -27,12 +28,12 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    static int argIndex = 0;
 
-    //Initialize variables
+
+    // Initialize variables
     struct my_data { // data structure that will store a name and the # of times it appears
         char name[30];
-        int count = 0;
+        int count;
     };
     struct my_data namecounts[100]; // array of 100 names & counts
 
@@ -40,10 +41,29 @@ int main(int argc, char *argv[]) {
     int currentSize = 0; //reflects the current size of the array
     char string[30]; //variable to hold strings read from fgets
 
+
+    // Setup pipe
+    int fd[2]; // first pipe to send args to children
+    if (pipe(fd)==-1) 
+    { 
+        fprintf(stderr, "Parent-Child Pipe Failed"); 
+        return 1; 
+    } 
+    
+    int fd1[2]; // second pipe to get children to send struct back
+    if(pipe(fd1)==-1)
+    {
+    	fprintf(stderr, "Child-Parent Pipe Failed");
+    	return 1;
+    }
+    
+    // Setup children processes
     pid_t pid;
-    // loop to instantiate children
+  
     for(int i = 1; i < argc; ++i)
     {
+
+	write(fd[1], argv[i], 100);
         pid = fork(); // forks a child
 
         if(pid < 0) // if fork results in error
@@ -51,27 +71,39 @@ int main(int argc, char *argv[]) {
             fprintf(stderr, "Pipe Failed");
             return 1;
         }
-        else if(pid = 0) // if child process
+        else if(pid == 0) // if child process
         {
-            argIndex++; // increment argIndex by 1
             break; // break out of loop to prevent forking
         }
 
     }
 
-    if(pid != 0) // if parent, do nothing
+    if(pid != 0) // if parent, add up structs and print
     {
+    	wait(NULL); // wait for one child to finish
+    	
+    	char string[30];
+    	read(fd1[0], string, 30); // read a name from pipe
+    	printf("%s", string);
+    	strcpy(namecounts[0].name, string);
+    	currentSize++;
         return 0;
     }
+    
+    // If not parent, run through this process
     //Open file
     FILE *textFile;
-    textFile = fopen(argv[argIndex], "r");
+    char fileName[100];
+    read(fd[0], fileName, 100);
+    textFile = fopen(fileName, "r");
     if(textFile == NULL) //edge case for when fopen fails to open a file
     {
-        fprintf(stderr, "error: cannot open file\n");
-        return 1;
-    }
 
+        fprintf(stderr, "range: cannot open file\n");
+        return 1;
+        
+    }
+    
     //This loop will read lines from the text file using fgets and will stop when the file has ended
     while(fgets(string, 30, textFile) != NULL) // loop until end of file
     {
@@ -82,37 +114,15 @@ int main(int argc, char *argv[]) {
             continue;
         }
         string[strcspn(string, "\n")] = 0; //formatting, removes trailing \n from string
-
-        //else, check if name is unique & add to array
-        int check = 1; //checker for duplicate
-
-        //This loop traverses the namecounts struct and checks for duplicates
-        for(int i = 0; i < currentSize; ++i)
-        {
-            if(strcmp(string, namecounts[i].name) == 0) //if string matches w/ a name in array
-            {
-                check = 0; //set check to 0
-                namecounts[i].count++; // increment by 1
-            }
-
-            if(!check)
-                break;
-        }
-        if(check == 1) //if check is still 1 after looping through array, add it to the array
-        {
-            strcpy(namecounts[currentSize].name, string); // add to namecounts data structure
-            namecounts[currentSize].count++; // increment by 1
-            currentSize++;
-        }
+	
+	// Write name to parent
+	write(fd1[1], string, 30);
     }
-
-    for(int i = 0; i < currentSize; ++i)
-    {
-        printf("%s: %d\n", namecounts[i].name, namecounts[i].count);
-    }
-
+    printf("Child %d is done! Arg was %s \n", getpid(), fileName);
     fclose(textFile);
+    exit(0);
 
     return 0;
 }
+
 
