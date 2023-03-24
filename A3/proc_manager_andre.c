16a402c663c2b76078f6a50d1104f4e9793da9af
andrej-rivera <andre_rivera1337@hotmail.com>
2023-03-23 20:22:02 -0700

    A3 - More changes to proc_manager_andre

--- A3/proc_manager_andre.c
+++ A3/proc_manager_andre.c
@@ -13,10 +13,6 @@ int main(int argc, char *argv[]) {
 
     char buf[100][30]; // buffer that stores 100 commands of length 30 chars
     int i = 0;
-
-    pid_t	pid;
-	int		status;
-	printf("%% ");
 	
 	//loop that takes in inputs
 	while (fgets(buf[i], 30, stdin) != NULL) {
@@ -24,39 +20,45 @@ int main(int argc, char *argv[]) {
 		if (buf[i][strlen(buf[i]) - 1] == '\n')
 			buf[i][strlen(buf[i]) - 1] = 0; /* replace newline with null */
         
-        if ((pid = fork()) < 0) {
-			//err_sys("fork error");
-		} else if (pid == 0) {		/* child */
-		    char* args[2];
-            args[0] = strtok(buf[i], " ");
-            //args[0][strlen(args[0]) - 1] = 0;
-            
-		    //args[1] = strtok(buf[i], " ");
-		    args[1][strlen(args[1]) - 1] = 0;
-		    
-		    int fd = open("test.txt", O_RDWR | O_CREAT | O_APPEND, 0777);
-		    dup2(fd, 1);
-		    
-			execvp(args[0], args);
-			close(fd);
-			fprintf(stderr, "couldn't execute: %s\n", buf[i]);
-			exit(127);
-		}
-
-		/* parent */
-		if ((pid = waitpid(pid, &status, 0)) < 0)
-			printf("err");
-        
-
 		i++;
-	    printf("%% ");
 	}
+	printf("\n");
 	
-	//Loop to print commands entered
-	printf("\nLines entered: ");
+	pid_t	pid;
+	
+	//Loop to run commands
 	for(int j = 0; j < i; ++j)
     {
-        printf("\n%s", buf[j]);
+        if ((pid = fork()) < 0) { // if error forking
+			//err_sys("fork error");
+		} 
+		else if (pid == 0) {		/* child */
+		    char* args[2];
+            args[0] = strtok(buf[j], " ");
+            //args[0][strlen(args[0]) - 1] = 0;
+            
+		    args[1] = strtok(buf[j], " ");
+		    //args[1][strlen(args[1]) - 1] = 0;
+		    
+		    char filename[30];
+		    sprintf(filename, "%d", getpid());
+		    int fdout = open(strcat(filename, ".out"), O_RDWR | O_CREAT | O_APPEND, 0777);
+		    sprintf(filename, "%d", getpid());
+		    int fderr = open(strcat(filename, ".err"), O_RDWR | O_CREAT | O_APPEND, 0777);
+		    dup2(fdout, 1);
+		    dup2(fderr, 2);
+		    
+		    fprintf(fdout, "Starting command %d: child %d pid of parent %d", j + 1, getpid(), getppid());
+		    
+			execvp(args[0], args);
+			fprintf(stdout, "Finished child %d pid of parent %d", getpid(), getppid());
+			fprintf(stderr, "Exited with exitcode = %d", exitcode);
+			close(fdout);
+			close(fderr);
+			exit(127);
+		}
     }
-    printf("\n");
+    
+    while (wait ( NULL) != -1) {  }
+    printf("All Done!\n");
 }
